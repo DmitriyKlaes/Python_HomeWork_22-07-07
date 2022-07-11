1d947165b828dc0fba7a4f7258d42f42ce588da5
Dmitriy_Klaes <89119584059@mail.ru>
2022-07-11 22:21:41 +0300

    task002 - проверка закона де Моргана

--- task001-truth_statement.py
+++ task001-truth_statement.py
@@ -1,9 +1,7 @@
 # Напишите программу для проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x, y, z = map(int, input('Введите три цифры через пробел: ').split())
-if not (x and y and z) == (not x) or (not y) or (not z):
-    print('True')
-    print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z}')
-else:
-    print('False')
+for x in (0, 1):
+    for y in (0, 1):
+        for z in (0, 1):
+            print((not(x and y and z)) == (not x or not y or not z))
