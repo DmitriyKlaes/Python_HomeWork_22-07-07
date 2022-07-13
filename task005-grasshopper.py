c36b1da8c05bb9cfb47a21c8d019db5fb8565420
Dmitriy_Klaes <89119584059@mail.ru>
2022-07-13 23:22:34 +0300

    task005 - задача про кузнечика (фибоначчи)

--- task005-grasshopper.py
+++ task005-grasshopper.py
@@ -12,20 +12,6 @@
 # 3 -> 2
 # 10 -> 55
 
-# pillar = input('Введите номер последнего столбика: ')
-# while not pillar.isdigit() or 1 > int(pillar) or int(pillar) > 45:
-#     pillar = input('Неверный ввод! Введите число от 1 до 45: ')
-# # Программа не выполняется, пока не введешь цифру ор 1 до 45
-# pillar = int(pillar)
-# start_pillar = 0
-# second_pillar = 1
-# result = 0
-# for i in range(pillar-1):
-#     result = start_pillar + second_pillar
-#     start_pillar = second_pillar
-#     second_pillar = result
-# print(result)
-
 def correct_value():  # Позволяет ввести в строку только числовое значение
     result = input('Введите номер последнего столбика: ')
     while not result.isdigit() or 1 > int(result) or int(result) > 45:
@@ -33,12 +19,30 @@ def correct_value():  # Позволяет ввести в строку толь
     return int(result)
 
 
-def pillars_way(n):
-    if n == 1 or 2:
-        return 1
-    else:
-        result = 0
-        return result + pillars_way(n-1) + pillars_way(n-2)
-
 pillar = correct_value()
-print(pillars_way(pillar))
+start_pillar, second_pillar = 0, 1
+for i in range(pillar-1):
+    result = start_pillar + second_pillar
+    start_pillar, second_pillar = second_pillar, result
+print(result)
+
+# Дальше закомменитрован рекурсивный метод поиска результата
+# Но он мне не понравился, так как слишком долгое вычисление дальше 25
+
+# def correct_value():  # Позволяет ввести в строку только числовое значение
+#     result = input('Введите номер последнего столбика: ')
+#     while not result.isdigit() or 1 > int(result) or int(result) > 45:
+#         result = input('Неверный ввод! Введите число от 1 до 45: ')
+#     return int(result)
+
+
+# def pillars_way(n):
+#     if n in [1, 2]:
+#         return 1
+#     else:
+#         result = pillars_way(n-1) + pillars_way(n-2)
+#         return result
+
+# pillar = correct_value()
+# result = pillars_way(pillar)
+# print(result)
